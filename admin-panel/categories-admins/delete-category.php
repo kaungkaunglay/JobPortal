2f52929ad05e72463a6e07692f0dd88fb395cf21
AungKhantZin <viennghink@outlook.com>
2022-12-31 23:14:55 +0630

    latest update

--- admin-panel/categories-admins/delete-category.php
+++ admin-panel/categories-admins/delete-category.php
@@ -1,1 +1,15 @@
 <?php
+require "../layouts/header.php";
+require "../../config/config.php";
+?>
+<?php
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $delete = $conn->prepare("DELETE FROM categories WHERE id = '$id'");
+        $delete->execute();
+        header("Location: ".ADMINURL."/categories-admins/show-categories.php");
+    }else{
+        header("Location: ".APPURL."/404.php");
+    }
+?>
+<?php require "../layouts/footer.php";  ?>
